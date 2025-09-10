5b6ed4554408e228d01892126e5254d9119f9a10
jdaremes <samesjemerad@gmail.com>
2025-09-10 12:42:51 -0600

    Taking stdio_init_all() out of infinite loop (Fixes exiting on test)

--- test/test.c
+++ test/test.c
@@ -24,9 +24,11 @@ void test_multiplication(void)
 
 int main (void)
 {
+    stdio_init_all();
+    sleep_ms(5000); // Give time for TTY to attach.
+    
     while(1) {
-        stdio_init_all();
-        sleep_ms(5000); // Give time for TTY to attach.
+        
         printf("Start tests\n");
         UNITY_BEGIN();
         RUN_TEST(test_variable_assignment);
